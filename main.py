e06e3c7848cba3dca48f02ec647b28afa82c3c0c
Michael Wittner <mwittner2@gmail.com>
2019-04-13 02:16:12 -0400

    added dataset and code

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from sklearn import datasets, svm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
-iris = datasets.load_iris()
-digits = datasets.load_digits()
+df = pd.read_csv("Demographic_Statistics_By_Zip_Code.csv")
+x_train = df[['PERCENT FEMALE']]
+y_train = df[['PERCENT RECEIVES PUBLIC ASSISTANCE']]
+x_test = x_train[len(x_train)-20:]
+y_test = y_train[len(y_train)-20:]
+x_train = x_train[:len(x_train)-20]
+y_train = y_train[:len(y_train)-20]
+print(x_train, y_train)
 
-# print(digits.data)
 
-clf = svm.SVC(gamma=0.001, C=100.)
 
-clf.fit(digits.data[:-1], digits.target[:-1])
 
-predictions = (clf.predict(digits.data[-1:]))
 
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(x_train, y_train)
+# predictions = np.array()
+# predictions = predictions.reshape(-1,1)
+# Make predictions using the testing set
+jurisdiction_prediction = regr.predict(x_test)
+# print(jurisdiction_prediction)
+
+# # Plot outputs
+plt.scatter(x_test, y_test,  color='black')
+plt.plot(x_test, jurisdiction_prediction, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
