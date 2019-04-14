6dee42f2de2362c88f162451d6a6d4a12746d9b2
Michael Wittner <mwittner2@gmail.com>
2019-04-13 21:00:26 -0400

    Final code + Readme updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bitcamp-ML-Workshop
 Code for the Introduction to Machine Learning at Bitcamp
+
+Slides: [here](https://docs.google.com/presentation/d/1WURguBHvTAFXhycNnSu9rta8LdqZx715kGEklUdOHRQ/edit?usp=sharing)
--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv("Demographic_Statistics_By_Zip_Code.csv")
-x_train = df[['PERCENT FEMALE']]
+x_train = df[['PERCENT MALE']]
 y_train = df[['PERCENT RECEIVES PUBLIC ASSISTANCE']]
+#train data (everything but last 20)
 x_test = x_train[len(x_train)-20:]
 y_test = y_train[len(y_train)-20:]
+print(x_train, y_train)
+
+#testing data (last 20)
 x_train = x_train[:len(x_train)-20]
 y_train = y_train[:len(y_train)-20]
-print(x_train, y_train)
 
 
 
@@ -23,15 +26,13 @@ regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
 regr.fit(x_train, y_train)
-# predictions = np.array()
-# predictions = predictions.reshape(-1,1)
-# Make predictions using the testing set
-jurisdiction_prediction = regr.predict(x_test)
-# print(jurisdiction_prediction)
 
-# # Plot outputs
+#Get predictions
+predictions = regr.predict(x_test)
+
+# Plot outputs
 plt.scatter(x_test, y_test,  color='black')
-plt.plot(x_test, jurisdiction_prediction, color='blue', linewidth=3)
+plt.plot(x_test, predictions, color='blue', linewidth=3)
 
 plt.xticks(())
 plt.yticks(())
